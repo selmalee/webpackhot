86fca3d49d8f3567689d5d89cf4229865c02d603
seminelee <747191197@qq.com>
2017-07-09 11:07:46 +0800

    change on 2017/07/09

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,8 +5,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: path.join(__dirname, 'src'),
     output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'bundle.js'
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
     },
     devServer: {
         inline: true,
